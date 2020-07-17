3ade6cfd22a134ffb7c3befb897c148d8a3e2059
mingzhang <redondoself@gmail.com>
2020-07-17 13:04:21 -0400

    matrix-plot started

---
+++ src/matrix-plot/js/index.js
@@ -0,0 +1,37 @@
+import drawContactMap from "./canvas/contact-map.js";
+import data from "./utils/sample-data.js";
+import setupDownload from "./utils/download.js";
+import setupImgSliding from "./utils/image-sliding.js";
+import load from "./utils/select-data-to-show.js";
+
+window.onload = () => {
+  load();
+  handleModeSwitch();
+  handleTabSwitch();
+  setupDownload();
+  setupImgSliding();
+};
+
+function handleTabSwitch() {
+  let sw = document.getElementsByClassName('switch-controller')[0];
+  for (let ctrl of document.getElementsByClassName('switcher')) {
+    ctrl.addEventListener('click', function() {
+      sw.classList.toggle('contact');
+      sw.classList.toggle('network');
+    });
+  }
+}
+
+function handleModeSwitch() {
+  let main = document.getElementsByClassName('main')[0];
+  let live = document.getElementById('live');
+  let gallery = document.getElementById('gallery');
+  live.addEventListener('click', () => {
+    main.classList.add('live');
+    main.classList.remove('gallery');
+  });
+  gallery.addEventListener('click', () => {
+    main.classList.remove('live');
+    main.classList.add('gallery');
+  });
+}
---
+++ src/matrix-plot/js/utils/config.js
@@ -0,0 +1,24 @@
+const config = {
+  backgroundColor: 'white',
+  lineWidth: 2,
+  lineDash: [1, 3],
+  gridWidth: 30,
+  circleRadius: 12,
+  textColor: '#555',
+  textHighlightColor: '#f66',
+  textMargin: 20,
+  font: '18px monospace',
+  lineColor: 'rgba(233, 234, 100, .6)',
+  lineHighlightColor: 'rgba(233, 234, 20, .98)',
+  circleColor: 'rgba(119,136,153, .8)',
+  circleHighlightColor: 'rgba(85,102,119, .8)',
+  // margin is the margin of the grids, which the distance between borders of the grids to the border of the canvas
+  margin: {
+    top: 100,
+    right: 80,
+    bottom: 80,
+    left: 100,
+  },
+};
+
+export default config;
